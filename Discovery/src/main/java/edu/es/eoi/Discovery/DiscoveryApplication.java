a6cbe5eeb1124949889f0aa38ebf0a40ddaf976b
Ismael Dominguez Moreno <ismaeldominguezmoreno@gmail.com>
2020-08-03 16:26:29 +0200

    Config, Discovery and gateway server commit

---
+++ Discovery/src/main/java/edu/es/eoi/Discovery/DiscoveryApplication.java
@@ -0,0 +1,20 @@
+package edu.es.eoi.Discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+@EnableDiscoveryClient
+@EnableAdminServer
+public class DiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryApplication.class, args);
+	}
+
+}
